1e88357a499e7598c0d5987bedafea3db3663b53
Ts-sound <1564264931@qq.com>
2023-11-10 10:13:09 +0800

    add big dndian

---
+++ component/src/common/big_endian.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace gomros {
+namespace common {
+template <class T>
+void BigEndianWrite(uint8_t *pos, T &data) {
+  size_t size = sizeof(data);
+  for (size_t i = 0; i < size; i++) {
+    pos[size - i - 1] = (uint8_t)(data >> 8 * i);
+  }
+}
+
+template <class T>
+T BigEndianRead(const uint8_t *pos) {
+  size_t size = sizeof(T);
+  uint32_t temp = 0x0;
+  for (size_t i = 0; i < size; i++) {
+    temp |= (pos[size - i - 1] << i * 8);
+  }
+  return *((T *)&temp);
+}
+}  // namespace common
+}  // namespace gomros
