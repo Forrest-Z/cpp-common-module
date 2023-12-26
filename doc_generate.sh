6cfeaa73420ff6db974967e3cd928a31c0189cc5
Ts-sound <1564264931@qq.com>
2023-12-26 11:21:24 +0800

    add doxygen

---
+++ doc_generate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+### 使用软件为doxygen,graphviz, 确保运行脚本前软件安装正常
+# sudo apt install doxygen
+# sudo apt install graphviz
+
+# 文档生成源路径 ./prebuilt/include/
+# 文档存放目标路径 ./__build/doc/
+
+mkdir -p ./__build/doc/
+
+doxygen doc_generate_cfg.doxy
